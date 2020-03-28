ad80aa6a21c90920a8441909d6cbb175d86a5987
Shivam Agrawal <shivam.agrawal@lntinfotech.com>
2020-03-28 20:24:27 +0530

    upated

---
+++ precache-manifest.4c14e31041ac10a87f6a4fd8c8f6ec4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e597a93fad4007acde4f85177bcfef",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "2bba6790aeb63db80b01",
+    "url": "/covid19/static/css/main.e874910b.chunk.css"
+  },
+  {
+    "revision": "ff00f63cded6d75a8fba",
+    "url": "/covid19/static/js/2.94c482ed.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid19/static/js/2.94c482ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bba6790aeb63db80b01",
+    "url": "/covid19/static/js/main.0abcd360.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  }
+]);
